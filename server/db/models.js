db63d08588888a893bd00dda26e7941696141271
Adam Green <adam.green@kcl.ac.uk>
2020-12-29 21:48:59 +0000

    Turnkey solution

--- server/db/models.js
+++ server/db/models.js
@@ -2,8 +2,10 @@ import mongoose from 'mongoose';
 
 // Mongoose doesn't use default promises ughhh...
 mongoose.Promise = Promise;
+console.log(process.env);
+console.log(`mongodb://${process.env.DBHOST ? process.env.DBHOST : 'localhost'}:27017/bfqoty2020`);
 mongoose.connect(
-    `mongodb://database:27017/bfqoty2020`, { useNewUrlParser: true, useUnifiedTopology: true });
+    `mongodb://${process.env.DBHOST}:27017/bfqoty2020`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 import { questionSchema, playerSchema } from './schema';
 
