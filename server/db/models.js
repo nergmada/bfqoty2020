86e025931feff3ba9d56b9818810a381df8f7fa7
Adam Green <adam.green@kcl.ac.uk>
2020-12-29 21:49:11 +0000

    finished

--- server/db/models.js
+++ server/db/models.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 // Mongoose doesn't use default promises ughhh...
 mongoose.Promise = Promise;
-console.log(process.env);
 console.log(`mongodb://${process.env.DBHOST ? process.env.DBHOST : 'localhost'}:27017/bfqoty2020`);
 mongoose.connect(
     `mongodb://${process.env.DBHOST}:27017/bfqoty2020`, { useNewUrlParser: true, useUnifiedTopology: true });
